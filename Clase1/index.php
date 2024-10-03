9d3e4123a413875d49c4a6c35bd8cbada1959417
diegocapri <capriglionediego@gmail.com>
2024-10-03 16:00:19 -0300

    index.php

---
+++ Clase1/index.php
@@ -0,0 +1,13 @@
+<?php   
+
+class Billetera {
+    public $tamaño;
+    public $peso;
+    public $color;
+    public $cantidadPlata;
+}
+
+$billeteraJorge = new Billetera();
+
+$billeteraJorge->tamaño = 'Grande';
+$billeteraJorge->peso = 100;
