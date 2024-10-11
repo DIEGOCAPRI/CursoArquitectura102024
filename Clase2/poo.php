b034fd95cb810baea7fa587973fb40eb1345ebf2
diegocapri <capriglionediego@gmail.com>
2024-10-10 21:11:45 -0300

    Class DB

--- Clase2/poo.php
+++ Clase2/poo.php
@@ -9,5 +9,21 @@ class Persona {
     private $tiene_mascota = false;
 }
 
+class Service {
+    private $database_name = 'cursoArquitectura';
+    private $database_pass = 123456;
+    private $database_user = 'Test';
+    private $database_host = 'localhost';
+
+    public function conectarBase  ($database_host, $database_name, $database_pass, $database_user){
+       return  $link = mysqli_connect($database_host,$database_user, $database_pass, $database_name);
+    }
+
+    public function executeDatabase($sql){
+        
+        $mysqli = $this->conectarBase( $this->$database_host, $this->$database_name, $this->$database_pass, $this->$database_user );
+        return $result = mysqli_execute_query($mysqli, $sql, null);
+    }
+}
 
 $juan = new Persona();
