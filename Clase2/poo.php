0158cb6f4f50bdbe69a5cbd6f336bf1ec320aef9
diegocapri <capriglionediego@gmail.com>
2024-10-18 15:05:20 -0300

    Incorporo Clases

--- Clase2/poo.php
+++ Clase2/poo.php
@@ -10,18 +10,15 @@ class Persona {
 }
 
 class Service {
-    private $database_name = 'cursoArquitectura';
-    private $database_pass = 123456;
-    private $database_user = 'Test';
-    private $database_host = 'localhost';
+    
 
-    public function conectarBase  ($database_host, $database_name, $database_pass, $database_user){
+    private function conectarBase  ($database_host, $database_name, $database_pass, $database_user){
        return  $link = mysqli_connect($database_host,$database_user, $database_pass, $database_name);
     }
 
     public function executeDatabase($sql){
         
-        $mysqli = $this->conectarBase( $this->$database_host, $this->$database_name, $this->$database_pass, $this->$database_user );
+        $mysqli = $this->conectarBase( 'cursoArquitectura', 123456, 'Test', 'localhost' );
         return $result = mysqli_execute_query($mysqli, $sql, null);
     }
 }
